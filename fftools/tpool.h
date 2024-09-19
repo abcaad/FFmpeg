0becf1f1adb3b03ad518701ebea463f855233c08
abcaad <68930639+abcaad@users.noreply.github.com>
2024-09-19 14:45:47 +0800

    tpool_fix

--- fftools/tpool.h
+++ fftools/tpool.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __TPOOL_H__
 #define __TPOOL_H__
 
@@ -17,23 +16,3 @@ bool tpool_add_work(tpool_t *tm, thread_func_t func, void *arg);
 void tpool_wait(tpool_t *tm);
 
 #endif /* __TPOOL_H__ */
-=======
-#ifndef __TPOOL_H__
-#define __TPOOL_H__
-
-#include <stdbool.h>
-#include <stddef.h>
-
-struct tpool;
-typedef struct tpool tpool_t;
-
-typedef void (*thread_func_t)(void *arg);
-
-tpool_t *tpool_create(size_t num);
-void tpool_destroy(tpool_t *tm);
-
-bool tpool_add_work(tpool_t *tm, thread_func_t func, void *arg);
-void tpool_wait(tpool_t *tm);
-
-#endif /* __TPOOL_H__ */
->>>>>>> efa08985e677972cde98930a1c63b7e0a8b95744
